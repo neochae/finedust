0ebe20473ec2843a21e2092c4cd8c2981ded5600
neo <neo.chae.lg@gmail.com>
2017-07-22 02:47:07 +0900

    selenium + driver(phantomjs, chrome)로 naver login -> cafe 접속하여 article 얻어오는 부분 초기 적용 server에서는 phantomjs를 이용하여 headless crawler구현
    naver crawling만을 위해서는 requst에 기반한 방식이나
    django의 crawler package 를 활용할 수 있으나
    화면 캡쳐, 동영상 녹화 등 부가적인 정보 수집을 위하여 crawler를
    selenium을 활용하여 적용

--- finedust/custom_data/crawler.py
+++ finedust/custom_data/crawler.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import time
 
-driver = webdriver.Chrome('../../chromedriver/mac/chromedriver')
-#driver = webdriver.PhantomJS('../../phantomjs/mac/bin/phantomjs')
-
+#driver = webdriver.Chrome('../../chromedriver/mac/chromedriver')
+driver = webdriver.PhantomJS('../../phantomjs/mac/bin/phantomjs')
 driver.implicitly_wait(3)
+
 driver.get('https://nid.naver.com/nidlogin.login')
+time.sleep(3)
 
 driver.find_element_by_name('id').send_keys('kaistsw2017')
 driver.find_element_by_name('pw').send_keys('naver111!')
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
-
 driver.get('http://cafe.naver.com/dustout2?iframe_url=/ArticleList.nhn%3Fsearch.clubid=29031243%26search.boardtype=L')
+driver.switch_to.frame("cafe_main");
+
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-#article = driver.find_element_by_xpath("//*/span/span[@class='aaa']")
-#article = driver.find_elements(By.XPATH, "//*/span/span[@class='aaa']")
-#print(article)
+notices = soup.select("td.board-list > span > span.aaa")
+print(len(notices))
+for n in notices:
+    print(n.text.strip())
+
 
