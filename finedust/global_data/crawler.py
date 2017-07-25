b0e1b223400e9dd4e8ca63fcce3eea5d47fd2e35
Hyeonwoo-Choi <hw.choi@kaist.ac.kr>
2017-07-25 20:08:38 +0900

    Modify global_data

--- finedust/global_data/crawler.py
+++ finedust/global_data/crawler.py
@@ -14,9 +14,26 @@ from finedust.settings.local_setting import *
 from finedust.util.screen import *
 
 #TODO, Crawling
+class GlobalDataCrawler :
+    def __init__(self,id):
+        self.basicUrl = 'https://api.waqi.info/api/feed/@'
+        self.id = id
+        self.lastUrl = '/obs.kr.json'
 
-resp = requests.get('https://api.waqi.info/api/feed/@1451/obs.kr.json', headers={'User-agent': 'Mozilla/5.0'})
-print(resp.text)
+    def get_globaldata(self):
+        resp = requests.get(self.basicUrl + self.id + self.lastUrl, headers={'User-agent': 'Mozilla/5.0'})
+        print(resp.text)
+
+    def start(self):
+        self.get_globaldata()
+        # self.basicUrl = 'https://api.waqi.info/api/feed'
+
+
+
+if __name__ == '__main__':
+    id = '1451'
+    crawler = GlobalDataCrawler(id)
+    crawler.start()
 
 # # response = urllib2.urlopen('https://api.waqi.info/api/feed/@1451/obs.kr.json')
 # js_location = pd.read_json('https://api.waqi.info/api/feed/@1451/obs.en.json')
