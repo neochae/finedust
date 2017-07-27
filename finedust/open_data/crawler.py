c65c651e8f148df3aaee9f0511fe194bb9f7d044
nanbean <nanbean@nanbean.net>
2017-07-27 15:15:20 +0900

    Add Korean regional data crawling

--- finedust/open_data/crawler.py
+++ finedust/open_data/crawler.py
@@ -15,28 +15,24 @@ sys.path.append(os.path.join(BASE_DIR, '..'))
 from finedust.settings.local_setting import *
 from finedust.util.screen import *
 
-# Connect to the database
-connection = pymysql.connect(host=DATABASES_MYSQL['HOST'],
-                             user=DATABASES_MYSQL['USER'],
-                             password=DATABASES_MYSQL['PASSWORD'],
-                             db='mydb',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
 class OpenDataCrawler :
     def __init__(self,id):
         self.basicUrl = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate='
         self.today = date.today().strftime('%Y-%m-%d')
         self.servieKey = '&ServiceKey=' + DATAGOKRSERVICEKEY
         self.lastUrl = '&ver=1.1&_returnType=json'
+        self.avgBasicUrl = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?itemCode=PM10&dataGubun=HOUR&searchCondition=WEEK&pageNo=1&numOfRows=1'
 
     def get_oepndata(self):
         resp = requests.get(self.basicUrl + self.today + self.servieKey + self.lastUrl, headers={'User-agent': 'Mozilla/5.0'})
-        # with open('air_korea_forecast_sample.json', 'rt', encoding='UTF8') as data_file:
-        #     resp = json.load(data_file)
         json_data = self.get_json(resp)
         json_normal_data = json_normalize(json_data['list'][0])
         self.updateDatabase(json_normal_data)
+
+        resp = requests.get(self.avgBasicUrl + self.servieKey + self.lastUrl, headers={'User-agent': 'Mozilla/5.0'})
+        json_data = self.get_json(resp)
+        json_normal_data = json_normalize(json_data['list'][0])
+        self.updateAvgDatabase(json_normal_data)
 
     def get_json(self, data):
         return json.loads(data.text)
@@ -44,8 +40,15 @@ class OpenDataCrawler :
     def updateDatabase(self, data):
         image = data['imageUrl7'][0]
         dateTime = data['dataTime'][0]
-        print(image)
-        print(dateTime)
+
+        # Connect to the database
+        connection = pymysql.connect(host=DATABASES_MYSQL['HOST'],
+                                     user=DATABASES_MYSQL['USER'],
+                                     password=DATABASES_MYSQL['PASSWORD'],
+                                     db='mydb',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
         try:
             with connection.cursor() as cursor:
                 # Read a single record
@@ -61,6 +64,61 @@ class OpenDataCrawler :
                         sql = "INSERT INTO `finedust_forecast` (`dateTime`, `image`) VALUES (%s, %s)"
                         print(sql)
                         cursor.execute(sql, (dateTime, image))
+
+                    # connection is not autocommit by default. So you must commit to save
+                    # your changes.
+                    connection.commit()
+        finally:
+            connection.close()
+
+    def updateAvgDatabase(self, data):
+        dateTime = data['dataTime'][0]
+        busan = data['busan'][0]
+        chungbuk = data['chungbuk'][0]
+        chungnam = data['chungnam'][0]
+        daegu = data['daegu'][0]
+        daejeon = data['daejeon'][0]
+        gangwon = data['gangwon'][0]
+        gwangju = data['gwangju'][0]
+        gyeongbuk = data['gyeongbuk'][0]
+        gyeonggi = data['gyeonggi'][0]
+        gyeongnam = data['gyeongnam'][0]
+        incheon = data['incheon'][0]
+        jeju = data['jeju'][0]
+        jeonbuk = data['jeonbuk'][0]
+        jeonnam = data['jeonnam'][0]
+        sejong = data['sejonggit'][0]
+        seoul = data['seoul'][0]
+        ulsan = data['ulsan'][0]
+
+        # Connect to the database
+        connection = pymysql.connect(host=DATABASES_MYSQL['HOST'],
+                                     user=DATABASES_MYSQL['USER'],
+                                     password=DATABASES_MYSQL['PASSWORD'],
+                                     db='mydb',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT * FROM `finedust_me` WHERE `dateTime`=%s"
+                cursor.execute(sql, (dateTime,))
+                result = cursor.fetchone()
+                # If the forecast doesn't exist in database, we should update the forecase
+                if (result) :
+                    print('Duplicated Data')
+                else :
+                    with connection.cursor() as cursor:
+                        # Create a new record
+                        sql = "INSERT INTO `finedust_me` (`dateTime`, `busan`, `chungbuk`," \
+                              " `chungnam`, `daegu`, `daejeon`, `gangwon`, `gwangju`," \
+                              " `gyeongbuk`, `gyeonggi`, `gyeongnam`, `incheon`, `jeju`," \
+                              " `jeonbuk`, `jeonnam`, `sejong`, `seoul`, `ulsan`)" \
+                              " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                        print(sql)
+                        cursor.execute(sql, (dateTime, busan, chungbuk, chungnam, daegu, daejeon, gangwon, gwangju, gyeongbuk,
+                                             gyeonggi, gyeongnam, incheon, jeju, jeonbuk, jeonnam, sejong, seoul, ulsan))
 
                     # connection is not autocommit by default. So you must commit to save
                     # your changes.
