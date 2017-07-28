f89bc4f122dc2abc1f46dca4c00f8881d7ff5555
neo <neo.chae.lg@gmail.com>
2017-07-29 00:49:58 +0900

    dbconnector 위치 변경

--- finedust/global_data/crawler.py
+++ finedust/global_data/crawler.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(BASE_DIR, '..'))
 
 from finedust.settings.local_setting import *
-from finedust.util.screen import *
+from finedust.util.dbconnector import *
 
 #TODO, Crawling
 class GlobalDataCrawler :
