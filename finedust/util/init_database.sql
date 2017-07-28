e88311b7810503a02f169b83d131c65b7d17d830
neo <neo.chae.lg@gmail.com>
2017-07-28 17:08:42 +0900

    Database 모델링 변경 및 초기 디폴트 데이터 값 일괄 적용 스크립트 생성
    mysql -uroot -p1234 < init_database.sql

---
+++ finedust/util/init_database.sql
@@ -0,0 +1,275 @@
+DROP SCHEMA IF EXISTS `mydb`;
+CREATE SCHEMA `mydb` ;
+
+-- MySQL Workbench Synchronization
+-- Generated: 2017-07-28 16:56
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: user
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE TABLE IF NOT EXISTS `mydb`.`custom_article` (
+  `article_id` INT(11) NOT NULL,
+  `writer` VARCHAR(45) NOT NULL,
+  `date` DATETIME NOT NULL,
+  `content` VARCHAR(512) NOT NULL,
+  `region` INT(11) NOT NULL,
+  `crawler` INT(11) NOT NULL,
+  `data` INT(11) NOT NULL,
+  PRIMARY KEY (`article_id`),
+  INDEX `data_idx` (`data` ASC),
+  INDEX `crawling_event_idx` (`crawler` ASC),
+  INDEX `region_idx` (`region` ASC),
+  UNIQUE INDEX `article_id_UNIQUE` (`article_id` ASC),
+  CONSTRAINT `region1`
+    FOREIGN KEY (`region`)
+    REFERENCES `mydb`.`region` (`region_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `crawling_event1`
+    FOREIGN KEY (`crawler`)
+    REFERENCES `mydb`.`crawling_event` (`crawling_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `data1`
+    FOREIGN KEY (`data`)
+    REFERENCES `mydb`.`finedust_data` (`data_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`region_category` (
+  `category_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`region` (
+  `region_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `eng_name` VARCHAR(45) NULL DEFAULT NULL,
+  `category` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`region_id`),
+  INDEX `category_idx` (`category` ASC),
+  CONSTRAINT `category`
+    FOREIGN KEY (`category`)
+    REFERENCES `mydb`.`region_category` (`category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`source_info` (
+  `source_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(512) NULL DEFAULT NULL,
+  `url` VARCHAR(512) NULL DEFAULT NULL,
+  PRIMARY KEY (`source_id`),
+  UNIQUE INDEX `url_UNIQUE` (`url` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`crawling_event` (
+  `crawling_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NOT NULL,
+  `source` INT(11) NOT NULL,
+  PRIMARY KEY (`crawling_id`),
+  INDEX `source_idx` (`source` ASC),
+  CONSTRAINT `source1`
+    FOREIGN KEY (`source`)
+    REFERENCES `mydb`.`source_info` (`source_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`finedust_data` (
+  `data_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `info` INT(11) NOT NULL,
+  `data_min` INT(11) NULL DEFAULT NULL,
+  `data_max` INT(11) NULL DEFAULT NULL,
+  `data_avg` INT(11) NOT NULL,
+  PRIMARY KEY (`data_id`),
+  INDEX `info_id_idx` (`info` ASC),
+  CONSTRAINT `info_id1`
+    FOREIGN KEY (`info`)
+    REFERENCES `mydb`.`finedust_info` (`info_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`finedust_info` (
+  `info_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`info_id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`open_api` (
+  `open_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `time` DATETIME NOT NULL,
+  `region` INT(11) NOT NULL,
+  `crawler` INT(11) NOT NULL,
+  `data` INT(11) NOT NULL,
+  PRIMARY KEY (`open_id`),
+  INDEX `data_idx` (`data` ASC),
+  INDEX `crawling_event_idx` (`crawler` ASC),
+  INDEX `region3_idx` (`region` ASC),
+  CONSTRAINT `region3`
+    FOREIGN KEY (`region`)
+    REFERENCES `mydb`.`region` (`region_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `data3`
+    FOREIGN KEY (`data`)
+    REFERENCES `mydb`.`finedust_data` (`data_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `crawling_event3`
+    FOREIGN KEY (`crawler`)
+    REFERENCES `mydb`.`crawling_event` (`crawling_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`telegram_user` (
+  `chat_id` INT(11) NOT NULL,
+  `start_date` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`chat_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`favorite_region` (
+  `favorite_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user` INT(11) NOT NULL,
+  `region` INT(11) NOT NULL,
+  PRIMARY KEY (`favorite_id`),
+  INDEX `region_idx` (`region` ASC),
+  INDEX `user_idx` (`user` ASC),
+  CONSTRAINT `user14`
+    FOREIGN KEY (`user`)
+    REFERENCES `mydb`.`telegram_user` (`chat_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `region4`
+    FOREIGN KEY (`region`)
+    REFERENCES `mydb`.`region_category` (`category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+-- crawling source
+INSERT INTO `mydb`.`source_info` (`source_id`, `desc`, `url`)
+	VALUES ('0', '국내 사용자 미세먼지 정보',  'http://cafe.naver.com/dustout2');
+
+INSERT INTO `mydb`.`source_info` (`source_id`, `desc`, `url`)
+	VALUES ('0', '중국 미세먼지 정보',  'http://aqicn.org/');
+
+INSERT INTO `mydb`.`source_info` (`source_id`, `desc`, `url`)
+	VALUES ('0', '국내 미세먼지 정보',  'http://openapi.airkorea.or.kr');
+
+
+-- crawling source
+INSERT INTO `mydb`.`finedust_info`
+	VALUES ('0', 'PM25'), ('0', 'PM10'), ('0', 'O3'), ('0', 'NO2'), ('0', 'SO2'), ('0', 'CO'), ('0', 'TEMP'),('0', 'DEW'),('0', 'PRESSURE'),('0', 'HUMID'),('0', 'WIND');
+
+-- region_category
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('1', '서울시');
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('2', '경기도');
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('3', '강원도');
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('4', '충청도');
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('5', '경상도');
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('6', '전라도');
+INSERT INTO `mydb`.`region_category` (`category_id`, `name`) VALUES ('7', '제주도');
+
+-- region
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('1', '동작, 관악', '동작, 관악', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('2', '성북, 강북, 도봉', '성북, 강북, 도봉', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('3', '광진, 성동, 동대문', '광진, 성동, 동대문', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('4', '송파, 강동', '송파, 강동', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('5', '마포, 은평, 서대문', '마포, 은평, 서대문', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('6', '구로, 금천, 영등포', '구로, 금천, 영등포', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('7', '강서, 양천', '강서, 양천', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('8', '강남, 서초', '강남, 서초', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('9', '노원, 중랑', '노원, 중랑', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('10', '종로, 중구, 용산', '종로, 중구, 용산', '1');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('11', '인천광역시', '인천광역시', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('12', '양평, 가평', '양평, 가평', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('13', '수원(영통,팔달,장안,권선)', '수원(영통,팔달,장안,권선)', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('14', '안산, 시흥', '안산, 시흥', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('15', '광주', '광주', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('16', '파주, 일산, 덕양', '파주, 일산, 덕양', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('17', '광명', '광명', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('18', '성남, 분당, 판교', '성남, 분당, 판교', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('19', '여주, 이천', '여주, 이천', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('20', '용인(처인,기흥,수지)', '용인(처인,기흥,수지)', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('21', '남양주, 구리, 하남', '남양주, 구리, 하남', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('22', '평택, 안성, 송탄', '평택, 안성, 송탄', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('23', '부천', '부천', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('24', '화성, 오산', '화성, 오산', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('25', '과천,안양,군포,의왕', '과천,안양,군포,의왕', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('26', '의정부, 양주, 장흥', '의정부, 양주, 장흥', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('27', '포천, 연천, 동두천', '포천, 연천, 동두천', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('28', '김포, 강화', '김포, 강화', '2');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('29', '원주, 문막, 횡성', '원주, 문막, 횡성', '3');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('30', '춘천, 홍천, 양구 ', '춘천, 홍천, 양구 ', '3');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('31', '평창, 정선, 태백, 영월', '평창, 정선, 태백, 영월', '3');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('32', '철원, 화천', '철원, 화천', '3');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('33', '강릉, 동해, 삼척', '강릉, 동해, 삼척', '3');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('34', '속초, 양양, 고성, 인제', '속초, 양양, 고성, 인제', '3');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('35', '대전광역시', '대전광역시', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('36', '보은, 옥천, 영동', '보은, 옥천, 영동', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('37', '충주, 제천, 단양', '충주, 제천, 단양', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('38', '음성, 진천, 괴산, 증평', '음성, 진천, 괴산, 증평', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('39', '청주', '청주', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('40', '세종시, 공주', '세종시, 공주', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('41', '천안, 아산', '천안, 아산', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('42', '청양, 예산', '청양, 예산', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('43', '서산, 당진, 태안', '서산, 당진, 태안', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('44', '논산, 계룡, 부여, 금산', '논산, 계룡, 부여, 금산', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('45', '보령, 홍성, 서천', '보령, 홍성, 서천', '4');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('46', '대구', '대구', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('47', '부산', '부산', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('48', '울산', '울산', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('49', '창원', '창원', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('50', '문경, 예천, 영주, 상주', '문경, 예천, 영주, 상주', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('51', '김천, 구미, 칠곡', '김천, 구미, 칠곡', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('52', '봉화, 울진, 영양, 양덕', '봉화, 울진, 영양, 양덕', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('53', '안동, 의성, 청송', '안동, 의성, 청송', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('54', '성주, 고령', '성주, 고령', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('55', '포항, 경주', '포항, 경주', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('56', '영천, 군위, 경산, 청도', '영천, 군위, 경산, 청도', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('57', '김해, 양산, 밀양', '김해, 양산, 밀양', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('58', '합천, 창녕, 의령, 함안', '합천, 창녕, 의령, 함안', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('59', '거창, 함양, 산청', '거창, 함양, 산청', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('60', '하동, 진주, 사천, 남해', '하동, 진주, 사천, 남해', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('61', '고성, 통영, 거제', '고성, 통영, 거제', '5');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('62', '광주', '광주', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('63', '부안, 정읍, 고창', '부안, 정읍, 고창', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('64', '임실, 남원, 순창', '임실, 남원, 순창', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('65', '진안, 무주, 장수', '진안, 무주, 장수', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('66', '군산, 익산, 김제', '군산, 익산, 김제', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('67', '전주, 완주', '전주, 완주', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('68', '영광, 장성, 담양, 함평', '영광, 장성, 담양, 함평', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('69', '신안, 무안, 목포, 영암', '신안, 무안, 목포, 영암', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('70', '곡성, 구례, 광양, 순천, 여수', '곡성, 구례, 광양, 순천, 여수', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('71', '나주, 화순, 보성, 고흥, 장흥', '나주, 화순, 보성, 고흥, 장흥', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('72', '영암, 강진, 해남, 진도, 완도', '영암, 강진, 해남, 진도, 완도', '6');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('73', '제주시', '제주시', '7');
+INSERT INTO `mydb`.`region` (`region_id`, `name`, `eng_name`, `category`) VALUES ('74', '서귀포시 ', '서귀포시 ', '7');
