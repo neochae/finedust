34074c75aea3bb47c515d6b395254977392ee42e
nanbean <nanbean@nanbean.net>
2017-07-29 15:54:05 +0900

    change open_data crawler according to database changes

--- finedust/open_data/crawler.py
+++ finedust/open_data/crawler.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import time
-from datetime import date
+import datetime
 import pymysql.cursors
 import requests
 import json
@@ -16,10 +16,31 @@ from finedust.settings.local_setting import *
 from finedust.util.screen import *
 from finedust.util.database import *
 
+region_dict = {
+    'busan': '부산',
+    'chungbuk': '충북',
+    'chungnam': '충남',
+    'daegu': '대구',
+    'daejeon': '대전',
+    'gangwon': '강원',
+    'gwangju': '광주',
+    'gyeongbuk': '경북',
+    'gyeonggi': '경기',
+    'gyeongnam': '경남',
+    'incheon': '인천',
+    'jeju': '제주',
+    'jeonbuk': '전북',
+    'jeonnam': '전남',
+    'sejong': '세종',
+    'seoul': '서울',
+    'ulsan': '울산'
+}
+
 class OpenDataCrawler :
     def __init__(self,id):
         self.basicUrl = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate='
-        self.today = date.today().strftime('%Y-%m-%d')
+        self.date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.today = datetime.datetime.now().strftime('%Y-%m-%d')
         self.servieKey = '&ServiceKey=' + DATAGOKRSERVICEKEY
         self.lastUrl = '&ver=1.1&_returnType=json'
         self.avgBasicUrl = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?itemCode=PM10&dataGubun=HOUR&searchCondition=WEEK&pageNo=1&numOfRows=1'
@@ -29,10 +50,10 @@ class OpenDataCrawler :
         self.dust_info = database_get_dust_info()
 
     def get_oepndata(self):
-        resp = requests.get(self.basicUrl + self.today + self.servieKey + self.lastUrl, headers={'User-agent': 'Mozilla/5.0'})
-        json_data = self.get_json(resp)
-        json_normal_data = json_normalize(json_data['list'][0])
-        self.updateDatabase(json_normal_data)
+        # resp = requests.get(self.basicUrl + self.today + self.servieKey + self.lastUrl, headers={'User-agent': 'Mozilla/5.0'})
+        # json_data = self.get_json(resp)
+        # json_normal_data = json_normalize(json_data['list'][0])
+        # self.updateDatabase(json_normal_data)
 
         resp = requests.get(self.avgBasicUrl + self.servieKey + self.lastUrl, headers={'User-agent': 'Mozilla/5.0'})
         json_data = self.get_json(resp)
@@ -77,25 +98,6 @@ class OpenDataCrawler :
             connection.close()
 
     def updateAvgDatabase(self, data):
-        dateTime = data['dataTime'][0]
-        busan = data['busan'][0]
-        chungbuk = data['chungbuk'][0]
-        chungnam = data['chungnam'][0]
-        daegu = data['daegu'][0]
-        daejeon = data['daejeon'][0]
-        gangwon = data['gangwon'][0]
-        gwangju = data['gwangju'][0]
-        gyeongbuk = data['gyeongbuk'][0]
-        gyeonggi = data['gyeonggi'][0]
-        gyeongnam = data['gyeongnam'][0]
-        incheon = data['incheon'][0]
-        jeju = data['jeju'][0]
-        jeonbuk = data['jeonbuk'][0]
-        jeonnam = data['jeonnam'][0]
-        sejong = data['sejonggit'][0]
-        seoul = data['seoul'][0]
-        ulsan = data['ulsan'][0]
-
         # Connect to the database
         connection = pymysql.connect(host=DATABASES_MYSQL['HOST'],
                                      user=DATABASES_MYSQL['USER'],
@@ -106,28 +108,22 @@ class OpenDataCrawler :
 
         try:
             with connection.cursor() as cursor:
-                # Read a single record
-                sql = "SELECT * FROM `finedust_me` WHERE `dateTime`=%s"
-                cursor.execute(sql, (dateTime,))
-                result = cursor.fetchone()
-                # If the forecast doesn't exist in database, we should update the forecase
-                if (result) :
-                    print('Duplicated Data')
-                else :
-                    with connection.cursor() as cursor:
-                        # Create a new record
-                        sql = "INSERT INTO `finedust_me` (`dateTime`, `busan`, `chungbuk`," \
-                              " `chungnam`, `daegu`, `daejeon`, `gangwon`, `gwangju`," \
-                              " `gyeongbuk`, `gyeonggi`, `gyeongnam`, `incheon`, `jeju`," \
-                              " `jeonbuk`, `jeonnam`, `sejong`, `seoul`, `ulsan`)" \
-                              " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                        print(sql)
-                        cursor.execute(sql, (dateTime, busan, chungbuk, chungnam, daegu, daejeon, gangwon, gwangju, gyeongbuk,
-                                             gyeonggi, gyeongnam, incheon, jeju, jeonbuk, jeonnam, sejong, seoul, ulsan))
+                for region_key, region_value in region_dict.items():
+                    # Create a new record
+                    sql = "INSERT INTO `finedust_data` " \
+                          "VALUES ('0'," + str(2) + "," + str(data[region_key][0]) + "," + str(data[region_key][0]) + "," + str(data[region_key][0]) + ")"
+                    print(sql)
+                    cursor.execute(sql)
+                    id = cursor.lastrowid
 
-                    # connection is not autocommit by default. So you must commit to save
-                    # your changes.
-                    connection.commit()
+                    sql = "INSERT INTO `open_api` " \
+                          "VALUES ('0'," + "'" + self.date + "'" + "," + str(self.region_info[region_value]) + "," + str(self.crawler_info) + "," + str(id) + ")"
+                    print(sql)
+                    cursor.execute(sql)
+
+                # connection is not autocommit by default. So you must commit to save
+                # your changes.
+                connection.commit()
         finally:
             connection.close()
 
